d399f38e2e48e095a6356d750ccc20532705bcd6
CodeWithNass <167813303+CodeWithNass@users.noreply.github.com>
2024-04-22 16:35:37 -0700

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import EnergyMixChart from "./components/EnergyMixChart";
+import { getEnergyData } from "./services/energyService";
+import "./app.css";
 
-const App = () => <h1>UK Energy Mix</h1>;
+function App() {
+  const [energyData, setEnergyData] = useState(null);
 
-export {
-    App
+  useEffect(() => {
+    fetchEnergyData();
+  }, []);
+
+  const fetchEnergyData = async () => {
+    try {
+      const data = await getEnergyData();
+      setEnergyData(data);
+    } catch (error) {
+      console.error("Error fetching energy data:", error);
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="app-header">UK Energy Generation</h1>
+      </header>
+      <main className="app-chart">
+        {energyData ? (
+          <EnergyMixChart data={energyData} />
+        ) : (
+          <h3>No data to display...</h3>
+        )}
+      </main>
+    </div>
+  );
 }
+
+export default App;
